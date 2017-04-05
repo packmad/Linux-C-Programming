770c268e1363266a43e44579cbf9910ba9d2d1ad
simoneaonzo <simone.aonzo@gmail.com>
2017-04-05 18:44:52 +0200

    fix2

---
+++ ese4/01_zombie.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t child_pid;
+
+    child_pid = fork();
+
+    if(child_pid != 0) { // parent
+        sleep(60);
+    }
+    else { // child
+		/*
+A zombie process is a process that has terminated but has not been cleaned up yet. It
+is the responsibility of the parent process to clean up its zombie children.
+*/
+        exit(0);
+    }
+    
+    return 0;
+}
---
+++ ese4/02_sigusr1.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+ * If you use a global variable to flag a signal from a signal-handler function, 
+ * it should be of the special type sig_atomic_t
+ * Linux guarantees that assignments to variables of this type are performed 
+ * in a single instruction and therefore cannot be interrupted midway
+ */
+sig_atomic_t sigusr1_count = 0;
+
+void sigusr1_handler(int signal_number)
+{
+    ++sigusr1_count;
+}
+
+int main()
+{
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &sigusr1_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    /** Main stuff here **/
+    pid_t pid = getpid();
+    printf("pid=%d\n", (int) pid);
+    sleep(120); // do something	
+
+    printf("The SIGUSR1 signal was raised for %d times.\n", sigusr1_count);
+
+    return 0;
+}
