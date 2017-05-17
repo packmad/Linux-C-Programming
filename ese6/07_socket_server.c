7f80682444980553bba2cd2a6118ab9f24be6ae0
simoneaonzo <simone.aonzo@gmail.com>
2017-05-17 12:33:10 +0200

    mini refactoring

--- ese6/07_socket_server.c
+++ ese6/07_socket_server.c
@@ -1,43 +1,51 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sysexits.h>
+#include <stdlib.h>
 
+#define BUF_SIZE 2048
+#define MAX_CONN 4
 #define PORT 8888
 
 int main(int argc, char *argv[])
 {
     int socket_desc, client_sock, c, read_size;
     struct sockaddr_in server , client;
-    char client_message[2000];
+    char client_message[BUF_SIZE];
 
-    //Create socket
+    // Create socket descriptor
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-        return -1;
+    if (socket_desc == -1) {
+        perror("Couldn't create socket");
+        exit(EX_OSERR);;
     }
     puts("Socket created");
 
-    //Prepare the sockaddr_in structure
+    // Prepare the sockaddr_sin structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( PORT );
+    server.sin_port = htons(PORT);
 
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
-        //print the error message
-        perror("bind failed. Error");
-        return -1;
+    // Bind
+    if(bind(socket_desc, (struct sockaddr *)&server , sizeof(server)) != 0) {
+        perror("Bind failed");
+        exit(EX_OSERR);;
     }
-    puts("bind done");
+    puts("Bind done");
 
-    //Listen
-    listen(socket_desc , 3);
+    /* 
+     * listen() marks the socket as a passive socket, that is, 
+     * as a socket that will be used to accept incoming connection
+     */
+    if (listen(socket_desc, MAX_CONN) != 0) {
+        perror("Listen failed");
+        exit(EX_OSERR);;
+	}
 
     //Accept and incoming connection
     puts("Waiting for incoming connections...");
@@ -47,7 +55,7 @@ int main(int argc, char *argv[])
     client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
     if (client_sock < 0) {
         perror("accept failed");
-        return -1;
+        exit(EX_OSERR);;
     }
     puts("Connection accepted");
 
